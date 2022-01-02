1a619cda5f357b639e472629f2e51c122ca0ca90
franco <x.francoruiz@gmail.com>
2022-01-02 19:44:40 -0300

    add example matchers test

---
+++ src/__tests__/example_matchers.test.js
@@ -0,0 +1,15 @@
+describe("matchers", () => {
+  test("toBe", () => {
+    expect(true).toBe(true);
+  });
+
+  test("toEqual", () => {
+    const data = { one: 1 };
+    data["two"] = 2;
+    expect(data).toEqual({ one: 1, two: 2 });
+  });
+
+  test("not", () => {
+    expect(true).not.toBe(false);
+  });
+});
