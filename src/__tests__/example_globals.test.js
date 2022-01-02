0eb59d422d1cc851c7acf0bb1581fe9300728ef4
franco <x.francoruiz@gmail.com>
2022-01-02 17:24:20 -0300

    add tests folder

---
+++ src/__tests__/example_globals.test.js
@@ -0,0 +1,9 @@
+describe("description group of test cases", () => {
+  it("description 1", () => {
+    expect(true).toBe(true);
+  });
+
+  it("description 2", () => {
+    expect(true).toBe(true);
+  });
+});
